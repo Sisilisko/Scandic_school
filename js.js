4ad050c0325656aa3a2df7f5e5614951883ce23f
Viki <beloborodova.v.v@yandex.ru>
2018-10-18 18:50:32 +0300

    js.js example

---
+++ js.js
@@ -0,0 +1,7 @@
+const e = document.querySelector("body")
+const el = document.querySelector("#responce-btn")
+
+el.addEventListener("mouseup", (event)=>{
+    console.log(event)
+    alert("hello world")
+})
