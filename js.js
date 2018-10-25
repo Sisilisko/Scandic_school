70583d11ffba11f75a49f8cd669b1afd20cf1cb2
Viki <beloborodova.v.v@yandex.ru>
2018-10-25 18:55:40 +0300

    summerschool_start

--- js.js
+++ js.js
@@ -1,7 +1,1 @@
-const e = document.querySelector("body")
-const el = document.querySelector("#responce-btn")
 
-el.addEventListener("mouseup", (event)=>{
-    console.log(event)
-    alert("hello world")
-})
